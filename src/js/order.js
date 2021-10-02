a80a78be452e03b0861dd85f8cd0700e4e70e0b5
vmikhalskyi <vladmikh31@gmail.com>
2021-10-02 16:52:44 +0300

    First changes for lab4

--- src/js/order.js
+++
@@ -1,4 +0,0 @@
-const orderNumber = document.querySelector('.success__number');
-const number = Math.round(Math.random() * 10000000);
-orderNumber.innerHTML = number;
-localStorage.removeItem('cart');
